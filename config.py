19744c87f3bf907bd9786f958de2597292d73ad3
Sajal Randhar <38528450+Sajal92@users.noreply.github.com>
2021-06-15 16:42:57 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import multiprocessing
 PORT = int(env.get("PORT", 8080))
 DEBUG_MODE = int(env.get("DEBUG_MODE", 1))
 
-# Gunicorn config
+# Gunicorn-config
 bind = ":" + str(PORT)
 workers = multiprocessing.cpu_count() * 2 + 1
 threads = 2 * multiprocessing.cpu_count()
